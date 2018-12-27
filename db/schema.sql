fe2f3787223ea786a09e8ef872d5ad102a880c42
nladha09 <ladha.noreen@gmail.com>
2018-12-26 18:57:45 -0700

    db setup

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+id INTEGER(10) AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+burger_name VARCHAR(255) NOT NULL,
+devoured BOOLEAN NOT NULL default 0,
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Burger', FALSE);
