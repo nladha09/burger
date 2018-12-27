4d2f2a9a67e190a24c4f7be73d1b2b28f5323289
nladha09 <ladha.noreen@gmail.com>
2018-12-27 10:07:49 -0700

    adding mySQL connection to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,20 @@
 // DEPENDENCIES // 
 
 var mysql = require('mysql');
-var connection;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-}	else {
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 	connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
 		user: 'root',
 		password: '',
 		database:'burgers_db'
-	});	
-}
+	});
 
 // Make connection.
 connection.connect(function(err) {
@@ -25,8 +26,9 @@ connection.connect(function(err) {
 	console.log("connected as id " + connection.threadId);
 });
 
-
-// connection.connect();
+app.listen(process.env.PORT || 3000, function() {
+	console.log("App listening on PORT " + PORT);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
