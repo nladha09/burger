09148f6d179b7f31ff3b180ab93054f63a91d223
nladha09 <ladha.noreen@gmail.com>
2018-12-29 15:09:22 -0700

    added css & server.js

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+
+var app = express();
+
+// serve static content for the app from the "public" directory in the application directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+
+}));
+
+// override with POST having ?_method = DELETE
+app.use(methodOverride('_method'));
+
+// handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+
+}));
+app.set('view engine', 'handlebars');
+
+
+// import routes and give the server across to them
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
