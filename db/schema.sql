a55cd2dce8778bdf1f33dff993c20ec09d6614ef
Noreen Ladha <ladha.noreen@gmail.com>
2019-01-03 14:50:33 -0700

    added jaws db connection to server

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,22 @@ burger_name VARCHAR (50) NOT NULL,
 devoured BOOLEAN DEFAULT false,
 date TIMESTAMP not null DEFAULT current_timestamp
 );
+
+SELECT * FROM burgers;
+
+/* JAWSDB schema --->
+
+USE vv63pqvjv716z0o9;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+id INTEGER(10) AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+burger_name VARCHAR (50) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP not null DEFAULT current_timestamp
+);
+
+SELECT * FROM burgers;
+
+*/
